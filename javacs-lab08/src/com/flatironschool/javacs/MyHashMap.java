930056114059092c85e67f22c7d8e5fe65823049
Eric Musyoka <ericmutua100@gmail.com>
2016-07-18 21:44:37 -0700

    Done.

--- javacs-lab08/src/com/flatironschool/javacs/MyHashMap.java
+++ javacs-lab08/src/com/flatironschool/javacs/MyHashMap.java
@@ -20,17 +20,58 @@ public class MyHashMap<K, V> extends MyBetterMap<K, V> implements Map<K, V> {
 	// average number of entries per map before we rehash
 	protected static final double FACTOR = 1.0;
 
+	//Fixing perfomance bug -  make size() take constant time
+	private int totalSize = 0;
+
 	@Override
 	public V put(K key, V value) {
+		MyLinearMap<K, V> chosenMap = chooseMap(key);
+		totalSize -= chosenMap.size();
 		V oldValue = super.put(key, value);
+		totalSize += chosenMap.size();
 		
 		//System.out.println("Put " + key + " in " + map + " size now " + map.size());
 		
 		// check if the number of elements per map exceeds the threshold
 		if (size() > maps.size() * FACTOR) {
+			totalSize = 0; //Fixing performance bug
 			rehash();
 		}
 		return oldValue;
+	}
+
+	/**
+	 * Fixing perfomance bug in clear() in MyBetterMap
+	 *
+	 */
+	@Override
+	public void clear() {
+		super.clear();
+		totalSize = 0;
+	}
+
+
+	/** 
+	 * Fixes perfomance bug in remove() in MyBetterMap
+	 *
+	 */
+	@Override
+	public V remove(Object key) {
+		MyLinearMap<K, V> map = chooseMap(key);
+		totalSize -= map.size();
+		V previous = map.remove(key);
+		totalSize += map.size();
+		return previous;
+	}
+
+
+	/**
+	 * Removes performance bug in size() iin MyBetterMap
+	 * 
+	 */
+	@Override
+	public int size() {
+		return totalSize;
 	}
 
 	/**
@@ -40,8 +81,15 @@ public class MyHashMap<K, V> extends MyBetterMap<K, V> implements Map<K, V> {
 	 * 
 	 */
 	protected void rehash() {
-        // TODO: fill this in.
-        throw new UnsupportedOperationException();
+		List<MyLinearMap<K, V>> previousMaps = maps;
+        int newSize = maps.size() * 2;
+        makeMaps(newSize);
+
+        for(MyLinearMap<K, V> map: previousMaps) {
+        	for(Map.Entry<K, V> entry: map.getEntries()) {
+        		put(entry.getKey(), entry.getValue());
+        	}
+        }    
 	}
 
 	/**
